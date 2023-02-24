3ebc69767b1ea4839720493d1e94a34bf380c1b1
VladLD <vladyslav.lapchynskyi@gmail.com>
2023-02-24 17:25:08 +0100

    AutolayoutHomeWorkApp

---
+++ AutolayoutHomeWorkApp/AutolayoutHomeWorkApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutolayoutHomeWorkApp
+//
+//  Created by Vlad Lapchynskyi on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var signInButton: UIButton!
+        
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        signInButton.layer.cornerRadius = 9
+       
+        
+    }
+}
+
